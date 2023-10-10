a88ef9fc5033691dc5ba27a514e3854faf3efc97
happy gikundiro <gikundirohappy@gmail.com>
2023-10-10 12:07:23 +0200

    renamed the filename

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import InputField from "./components/inputField";
 import { Todo } from "./model";
-import TodoList from "./components/todolist";
+import TodoList from "./components/todoList";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -24,12 +24,6 @@ const App: React.FC = () => {
       <h1 className="text-center text-4xl">Taskify</h1>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       <TodoList todos={todos} setTodos={setTodos} />
-      {/* <p className="center bg-red-300 p-40">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum,
-        provident accusamus. Eius laborum tenetur eum ad pariatur deleniti
-        quidem cum eligendi ullam dignissimos minus optio suscipit perferendis
-        fugiat, distinctio esse.
-      </p> */}
     </div>
   );
 };
